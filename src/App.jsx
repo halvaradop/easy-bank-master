b362cdda05d0bbc43f1f45daa400f6a94f6df57f
halvaradop_ <hernanvid123@gmail.com>
2023-06-12 09:56:09 -0500

    footer section complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
+import { Footer } from "./components/Footer"
 
 const App = () => {
-  
+    return (
+        <>
+            <Footer/>
+        </>
+    )
 }
 
 export default App
---
+++ src/utils/data.js
@@ -0,0 +1,10 @@
+import facebook from "../assets/facebook.svg"
+import youtube from "../assets/youtube.svg"
+import twitter from "../assets/twitter.svg"
+import pinterest from "../assets/pinterest.svg"
+import instagram from "../assets/instagram.svg"
+
+
+const companies = [ facebook, youtube, twitter, pinterest, instagram ]
+
+export { companies }
